6dc41e65a427d2be9dd16ff7c2a56597ac3b35ec
RuthiSalant <RuthiSalant@gmail.com>
2022-05-11 20:16:49 +0300

    huser.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import { Login } from './components/login';
+import { Routes, Route, Switch, BrowserRouter } from 'react-router-dom'
+import{React}  from "react";
+import {TestList} from './components/myTests';
+ import PageNotFound from'./components/PageNotFound'
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Login />} />
+        <Route path='/myTests' exact element={<TestList />} />
+        <Route path='*' exact element={<PageNotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
